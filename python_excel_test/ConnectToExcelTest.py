84bdcf9cd4499f4eccf7ba53ec8301663c98ba3f
Yiunhi-LeeWoonghee <yi.unhi.yi@gmail.com>
2021-04-09 00:51:14 +0900

    2021/04/09 excel読込み_ディレクトリ指定テスト中

--- python_excel_test/ConnectToExcelTest.py
+++ python_excel_test/ConnectToExcelTest.py
@@ -1,8 +1,8 @@
 # openpyxlライブラリが必要
 # ターミナルにて pip install openpyxl 実行
-
-# no moduleエラー解消（openpyxlライブラリ格納先を明視）
+# コマンドライン引数取得
 import sys
+# no moduleエラー解消（ライブラリ格納先を明視）
 sys.path.append('c:\\users\\thele\\appdata\\local\\programs\\python\\python37\\lib\\site-packages')
 
 # pythonとexcelの連携ライブラリインポート
@@ -10,9 +10,28 @@ import openpyxl
 # 複数ファイルを処理するため、globライブラリをインポート
 import glob
 
-# ディレクトリ指定
-files = glob.glob('.\\excel_test\\*.xlsx')
+# コマンドライン引数一覧
+# args[0] = python実行ファイル名
+# args[1] = excelファイル格納先（ディレクトリ）
+# args[2] = 結果物出力先（ディレクトリ）
 
-# フルパス指定
-# workbook = openpyxl.load_workbook('D:\\devStudy\\PythonStudySpace\\excel_test\\*.xlsx')
-# print(workbook)
+# 引数を受け取る
+# args = sys.argv
+# execFileName = args[0]
+# excelDir = args[1]
+# resultDir = args[2]
+
+execFileName = "ConnectToExcelTest.py"
+excelDir = "D:\\devStudy\\PythonStudySpace\\excel_test"
+resultDir = "D:\\devStudy\\PythonStudySpace\\excel_tes\\result"
+
+
+# ディレクトリ指定
+# files = glob.glob(excelDir + "\\*.xlsx")
+#for excelfile in files
+#    print(excelfile)
+
+#for file in [files]
+#    print(file)
+#workbook = openpyxl.load_workbook('D:\\devStudy\\PythonStudySpace\\excel_test\\*.xlsx')
+#print(workbook)
