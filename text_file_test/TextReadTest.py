f3db81ae68f8abf32b2b964b4ce3b449df75cd87
Yiunhi-LeeWoonghee <yi.unhi.yi@gmail.com>
2021-04-13 01:43:42 +0900

    2021/04/13 テキストファイルの読み込み、及び正規表現で区切る処理追加

--- text_file_test/TextReadTest.py
+++ text_file_test/TextReadTest.py
@@ -1,9 +1,23 @@
 # osライブラリ
 from os import path
+# 正規表現の標準ライブラリ
+import re
 
 # 同じディレクトリのtxtファイルを開く
 file_path = path.join(path.dirname(__file__), 'Table_list.txt')
+
+# 変数の宣言のみ
+line = [] # 行のリスト
+s_list = [] # 区切りした文字列のリスト
+table_list = [] # テーブル名リスト
+rCount_list = [] # データ件数リスト
+
 with open(file_path, 'r') as f:
     # テキストファイルを読み込む
-    s = f.read().splitlines()
-    print(s)
+    line = f.read().splitlines()
+for reg_str in line:
+    s_list = re.split('\t', reg_str)
+    # 注意：appendの戻り値はNoneのため、変数で受け取れない（自動でリストに追加される）
+    table_list.append(s_list[0]) # テーブル名
+    rCount_list.append(s_list[1]) # テーブルのデータ件数
+
