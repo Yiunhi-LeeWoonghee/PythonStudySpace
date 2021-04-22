daa60ca66d9cbe7a928c8a1656fb407aa0ad9edb
Yiunhi-LeeWoonghee <yi.unhi.yi@gmail.com>
2021-04-22 23:58:39 +0900

    2021/04/22 ブロックチェーン作り練習その1（勉強）、現在作成中

---
+++ blockchain/blockchain_test.py
@@ -0,0 +1,19 @@
+"""仮想のブロックチェーンを作ってみる"""
+
+class Blockchain(object()):
+    
+    def __init__(self):
+        self.chain=[]
+        self.current_transactions=[]
+        
+        def new_block(self):
+            """新しいブロックを生成してチェーンに入る"""
+            pass
+    
+    #@staticmethod()
+    #def hash
+    
+    @property()
+    def last_block(self):
+        """チェーンの最後ブロックを返却"""
+        pass
