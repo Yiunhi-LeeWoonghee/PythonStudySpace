6c10210b792ce517a5b3e96f64f843b2d6712fdf
Yiunhi-LeeWoonghee <yi.unhi.yi@gmail.com>
2021-04-25 23:58:01 +0900

    2021/04/25 ブロックチェーン作り 取引結果のインデックス返却について

--- blockchain/blockchain_test.py
+++ blockchain/blockchain_test.py
@@ -19,3 +19,25 @@ class Blockchain(object()):
     def last_block(self):
         """チェーンの最後ブロックを返却"""
         pass
+    
+    def new_transaction(self, sender, recipient, amount):
+        """
+        リストに取引を追加し、追加されるブロックのインデックスを返却する
+        
+        sender : 送信者のアドレス
+        recipient : 受信者のアドレス
+        amount :
+        
+        return : 当取引のブロック数
+        """
+        self.current_transactions.append({
+                'sender' : sender,
+                'recipient' : recipient,
+                'amount' : amount,
+            })
+        
+        return self.last_block['index'] + 1
+    
+    def new_block(self, proof, previous_hash=None):
+        #return block
+        print()
